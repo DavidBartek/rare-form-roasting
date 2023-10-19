978c171a4f6f40a67adc2adfa6091fcfe03c424c
DavidBartek <davidwbartek@gmail.com>
2023-10-19 11:17:46 -0500

    closes #9 - user can delete addresses linked to his account (soft delete)

--- client/src/components/profile/ProfileAddresses.js
+++ client/src/components/profile/ProfileAddresses.js
@@ -1,79 +1,114 @@
 import { useState } from "react";
-import { Button, Table } from "reactstrap";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader, Table } from "reactstrap";
 import ProfileAddressEdit from "./ProfileAddressEdit";
-import { getAddressDetails } from "../../managers/addressManager";
+import { getAddressDetails, removeAddress } from "../../managers/addressManager";
 
 export default function ProfileAddresses ({ userDetails, renderUserDetails }) {
-
     const [editAddressView, setEditAddressView] = useState(false);
     const [addressToEdit, setAddressToEdit] = useState({});
+    const [addressToDelete, setAddressToDelete] = useState({});
+    const [modal, setModal] = useState(false);
 
     const handleModifyAddress = (e, a) => {
         e.preventDefault();
         getAddressDetails(a.id)
             .then(setAddressToEdit)
             .then(() => setEditAddressView(!editAddressView));
-        // does this need to be a separate "get" fetch to get the individual fully-composed address obj?
-        // pass in 'a' obj; fetch based on id; .then set address to edit view
-        // setEditAddressView(!editAddressView);
     }
 
-    const handleRemoveAddress = (e) => {
+    const handleRemoveAddress = (e, a) => {
         e.preventDefault();
-        console.log("delete address modal appears")
+        getAddressDetails(a.id)
+            .then(setAddressToDelete)
+            .then(toggle);
+    }
+
+    const toggle = () => setModal(!modal);
+
+    const confirmChanges = (e) => {
+        e.preventDefault();
+        removeAddress(addressToDelete.id)
+            .then(() => {renderUserDetails()})
+            .then(toggle);
     }
 
     if (!userDetails) {
         return null
     }
     return (
-        <Table borderless>
-            <thead>
-                <tr>
-                    <th>
-                        Linked Addresses
-                    </th>
-                </tr>
-            </thead>
-            {userDetails.shippingAddresses?.length > 0 ? (
-                editAddressView ? (
-                    <ProfileAddressEdit addressToEdit={addressToEdit} setEditAddressView={setEditAddressView} renderUserDetails={renderUserDetails}/>
+        <>
+            <Table borderless>
+                <thead>
+                    <tr>
+                        <th>
+                            Linked Addresses
+                        </th>
+                    </tr>
+                </thead>
+                {userDetails.shippingAddresses?.length > 0 ? (
+                    editAddressView ? (
+                        <ProfileAddressEdit addressToEdit={addressToEdit} setEditAddressView={setEditAddressView} renderUserDetails={renderUserDetails}/>
+                    ) : (
+                        <tbody>
+                        {userDetails.shippingAddresses.map(a =>
+                            <tr key={a.id}>
+                                <td>
+                                    {a.address1}<br />
+                                    {a.address2 ? (
+                                        <div>
+                                            {a.address2}
+                                            <br />
+                                        </div>
+                                        ) : (
+                                        ""
+                                        )}
+                                    {a.city}, {a.stateCode} {a.zip}
+                                </td>
+                                <td>
+                                    <Button onClick={(e) => {
+                                        handleModifyAddress(e, a);}}>Modify</Button>
+                                </td>
+                                <td>
+                                    <Button onClick={(e) => handleRemoveAddress(e, a)}>Remove</Button>
+                                </td>
+                            </tr>)}
+                    </tbody>
+                    )
                 ) : (
                     <tbody>
-                    {userDetails.shippingAddresses.map(a =>
-                        <tr key={a.id}>
+                        <tr>
                             <td>
-                                {a.address1}<br />
-                                {a.address2 ? (
-                                    <div>
-                                        {a.address2}
-                                        <br />
-                                    </div>
-                                    ) : (
-                                    ""
-                                    )}
-                                {a.city}, {a.stateCode} {a.zip}
+                                No linked addresses
                             </td>
-                            <td>
-                                <Button onClick={(e) => {
-                                    handleModifyAddress(e, a);}}>Modify</Button>
-                            </td>
-                            <td>
-                                <Button onClick={(e) => handleRemoveAddress(e)}>Remove</Button>
-                            </td>
-                        </tr>)}
-                </tbody>
-                )
-            ) : (
-                <tbody>
-                    <tr>
-                        <td>
-                            No linked addresses
-                        </td>
-                    </tr>
-                </tbody>
-            )}
-            
-        </Table>
+                        </tr>
+                    </tbody>
+                )}
+            </Table>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader>
+                    Confirm Removal?
+                </ModalHeader>
+                <ModalBody>
+                    {addressToDelete.address1}<br />
+                    {addressToDelete.address2 ? (
+                        <div>
+                            {addressToDelete.address2}
+                            <br />
+                        </div>
+                        ) : (
+                        ""
+                    )}
+                    {addressToDelete.city}, {addressToDelete.stateCode} {addressToDelete.zip}<br />
+                </ModalBody>
+                <ModalFooter>
+                    <Button onClick={(e) => confirmChanges(e)}>
+                        Confirm
+                    </Button>
+                    <Button onClick={toggle}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        </>
     )
 }
--- client/src/managers/addressManager.js
+++ client/src/managers/addressManager.js
@@ -2,7 +2,7 @@ const _apiUrl = "/api/shippingaddress";
 
 export const getAddressDetails = (addressId) => {
     return fetch(`${_apiUrl}/${addressId}`).then((res) => res.json());
-}
+};
 
 export const updateAddressDetails = (updatedAddress) => {
     return fetch(`${_apiUrl}/${updatedAddress.id}`, {
@@ -11,5 +11,11 @@ export const updateAddressDetails = (updatedAddress) => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(updatedAddress)
+    });
+};
+
+export const removeAddress = (addressId) => {
+    return fetch(`${_apiUrl}/${addressId}`, {
+        method: "DELETE"
     });
 };
