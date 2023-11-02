f5470068597f5443b2373c8c8e81e05de2ba05c6
DavidBartek <davidwbartek@gmail.com>
2023-11-02 15:44:24 -0600

    user's order history styled

--- client/src/components/userOrders/UserOrdersList.js
+++ client/src/components/userOrders/UserOrdersList.js
@@ -30,10 +30,10 @@ export default function UserOrdersList ({ loggedInUser }) {
         <Container>
             <h1>Order History</h1>
             <Table hover>
-                <tbody>
+                <tbody style={{border: "5px #FDE6FE solid"}}>
                     {myOrders.map(o => 
                     <tr key={o.id}>
-                        <td>
+                        <td className="textDetailsInList">
                             Order # {o.id}<br />
                             Placed {dateTimeConverter(o.datePlaced)}<br />
                             Total: ${priceFormatter(o.totalPrice)}<br />
@@ -41,16 +41,25 @@ export default function UserOrdersList ({ loggedInUser }) {
                         </td>
                         <td>
                             <br />
-                            <Button onClick={(e) => viewOrderDetails(e, o)}>
+                            <Button onClick={(e) => viewOrderDetails(e, o)} className="button" style={{
+                                backgroundColor: "#75BCFA",
+                                color: "#021E36",
+                                fontWeight: 800,
+                                border: "none",
+                                borderRadius: "0px",
+                                transition: "box-shadow 0.1s",
+                                fontSize: "larger"
+                                }}>
                                 View Details
-                                <BsCaretRight />
-                            </Button>
+                                <BsCaretRight className="textReset" style={{marginBottom: "2px"}}/>
+                            </Button >
                         </td>
                     </tr>    
                 )}    
                 </tbody>
             </Table>
         </Container>
+        
         <Offcanvas direction="end" isOpen={offCanvas} toggle={() => toggleOffCanvas()}>
             <UserOrderDetails order={orderWithDetails} toggleOffCanvas={toggleOffCanvas} />
         </Offcanvas>
