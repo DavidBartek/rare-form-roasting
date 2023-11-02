60bfb3824fdc6b3c4b5ef941d51be20a17010f68
DavidBartek <davidwbartek@gmail.com>
2023-11-02 09:36:50 -0600

    footer link implemented

--- client/src/components/navbar/Footer.js
+++ client/src/components/navbar/Footer.js
@@ -1,9 +1,18 @@
 import { BsEnvelope, BsInstagram } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 export default function Footer () {
+    const navigate = useNavigate();
+
+    const handleNavHome = (e) => {
+        e.preventDefault();
+        navigate("/");
+        window.scrollTo(0, 0);
+    }
+
     return (
         <div className="footer">
-            <img src="/Logo_Circle_Orange_1000x1000.svg" alt="circle logo" />
+            <img className="footerLogo" onClick={(e) => handleNavHome(e)}src="/Logo_Circle_Orange_1000x1000.svg" alt="circle logo" />
             <div className="footerLinks">
                 <img src="/Logo_Plane.svg" alt="logo"/>
                 <BsInstagram className="footerLink"/>
